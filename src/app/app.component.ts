53b39e87ce74dd7a48310b41916aafba3367f994
Jeryluxshigan <jeryluxshiganp@fortude.co>
2023-05-17 20:05:53 +0530

    First Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
+
 })
 export class AppComponent {
-  title = 'kendo-learning';
+  public kendokaAvatar =
+    "https://www.telerik.com/kendo-angular-ui-develop/components/navigation/appbar/assets/kendoka-angular.png";
+
+  public margin = { horizontal: -46, vertical: 7 };
+  public show = false;
+
+  public onToggle(): void {
+    this.show = !this.show;
+  }
+
+  
+  
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { MenuModule } from '@progress/kendo-angular-menu';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { IndicatorsModule } from '@progress/kendo-angular-indicators';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+
+
+
+
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +34,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    LayoutModule,
+    MenuModule,
+    NavigationModule,
+    IconsModule,
+    IndicatorsModule,
+    PopupModule,
+    InputsModule,
+    LabelModule,
+    DateInputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
